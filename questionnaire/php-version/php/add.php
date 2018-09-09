ce5138bca50cad755088d26d10ea308983a220f3
FKYnJYQ <loveress01@gmail.com>
2018-09-09 17:34:05 +0800

    增加预处理,防止数据库注入

--- questionnaire/php-version/php/add.php
+++ questionnaire/php-version/php/add.php
@@ -7,16 +7,19 @@ $conn = new mysqli("db", "root", "password", "db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO score(s_name, s_score, s_time) VALUES('" . $name . "', " . $score . "," . $time . ")";
-if ($conn->query($sql) == true) {
-    echo "seccess";
-} else {
-    echo "fail";
-}
-
-// if($stmt->execute())
-// {
-//     ech Uncaughto "success";
+// $sql = "INSERT INTO score(s_name, s_score, s_time) VALUES('" . $name . "', " . $score . "," . $time . ")";
+// if ($conn->query($sql) == true) {
+//     echo "seccess";
 // } else {
-//     echo "failed";
-// };
+//     echo "fail";
+// }
+
+// 尝试加入prepare 以防止注入
+
+if($stmt = $conn->prepare("INSERT INTO score(s_name, s_score, s_time) VALUES(?,?,?)")){
+    if(!$stmt->bind_param("sdi",$name,$score,$time)){
+        die("bind_param return error!");
+    }
+    $stmt->execute();
+    $stmt->close();
+};
