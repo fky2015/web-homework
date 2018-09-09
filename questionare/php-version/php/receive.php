ffdc9d52dc33105a9616cbd0b92ccb6a38faa2d7
FKYnJYQ <loveress01@gmail.com>
2018-09-09 15:55:49 +0800

    remove old folder

--- questionare/php-version/php/receive.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$name = $_POST["name"];
-$score = $_POST["score"];
-$time = $_POST["time"];
--- questionare/php-version/sql/init.sql
+++
@@ -1,7 +0,0 @@
-create database if not exists `db`; 
-use db;
-create table if not exists `score`(
-    s_name varchar(128),
-    s_score double(10,2),
-    s_time int(10)
-) ;
